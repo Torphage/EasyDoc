6cfc92601b98c313fe540aeb2446bc60887f788c
Torphage <filip.torphage1@gmail.com>
2018-09-30 21:06:48 +0200

    WOP get trigger from active file

--- src/easydoc.ts
+++ src/easydoc.ts
@@ -5,25 +5,40 @@ import * as path from 'path';
 
 class EasyDoc {
     config: any;
+    document: vs.TextDocument;
 
     constructor() {
-        this.config = vs.workspace.getConfiguration('autoDocstring');
+        this.config = vs.workspace.getConfiguration("EasyDoc");
+        this.document = vs.window.activeTextEditor.document;
     }
 
     public createDoc() {
         let dir = vs.extensions.getExtension('Torphage.easydoc').extensionPath + '/templates';
-
         let customFiles = this.dirSync(dir);
-        console.log(customFiles);   
+        customFiles.forEach(files => {
+            let trigger = this.config.get(files)
+            this.getEditorText(3);
+            // if (trigger.length) {
+                
+            // }
+        });
+        console.log(customFiles);  
     }
 
     // https://gist.github.com/kethinov/6658166
     private dirSync(dir, filelist = []) {
         fs.readdirSync(dir).forEach(file => {
             const dirFile = path.join(dir, file);
-            if (dirFile.includes('.')) filelist = [...filelist, path.basename(dirFile)]
+            if (dirFile.includes('.')) filelist = [...filelist, path.basename(dirFile, '.txt')]
         });
         return filelist;
+    }
+
+    private getEditorText(length: number) {
+        let regex = new RegExp('(\###)')
+        let test = this.document.getWordRangeAtPosition(this.document.positionAt(2), regex);
+        console.log(test);
+        // this.document.getText(3)
     }
 }
 
