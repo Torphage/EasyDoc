9271d2125b1d756e382330647f74c2005e2a570d
itggot-filip-torphage <filip.torphage@itggot.se>
2018-10-03 14:47:23 +0200

    added functions, better custom files support

--- src/easydoc.ts
+++ src/easydoc.ts
@@ -1,32 +1,41 @@
 import * as vs from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
+import { Format } from './format';
 
 
 class EasyDoc {
-    config: any;
-    document: vs.TextDocument;
+    config: vs.WorkspaceConfiguration;
+    document: vs.TextEditor;
+    dir: string;
 
     constructor() {
-        this.config = vs.workspace.getConfiguration("EasyDoc");
-        this.document = vs.window.activeTextEditor.document;
+        this.config = vs.workspace.getConfiguration('EasyDoc');
+        this.document = vs.window.activeTextEditor;
+        this.dir = vs.extensions.getExtension('Torphage.easydoc').extensionPath;
     }
-
-    public createDoc() {
-        let dir = vs.extensions.getExtension('Torphage.easydoc').extensionPath + '/templates';
-        let customFiles = this.dirSync(dir);
-        customFiles.forEach(files => {
-            let trigger = this.config.get(files)
-            this.getEditorText(3);
-            // if (trigger.length) {
-                
-            // }
+    
+    public checkDoc() {
+        let customFiles = this.dirSync(this.dir + '/templates');
+        customFiles.forEach(fileName => {
+            let packageFiles = this.getPackageJSON().contributes.configuration.properties;
+            if (!('EasyDoc.' + fileName in packageFiles)) {
+                this.writeConfig(fileName)
+            }
+            let fileConfig: any = this.config.get(fileName);
+            let triggerText = fileConfig.triggerString;
+            if (this.getEditorText(triggerText) === triggerText) {
+                console.log(triggerText);
+                let filePath = path.join(this.dir + '/templates', fileName + '.txt');
+                let format = new Format(filePath, fileConfig);
+                format.createDoc();
+            }
         });
-        console.log(customFiles);  
     }
 
     // https://gist.github.com/kethinov/6658166
-    private dirSync(dir, filelist = []) {
+    private dirSync(dir: string): string[] {
+        let filelist = [];
         fs.readdirSync(dir).forEach(file => {
             const dirFile = path.join(dir, file);
             if (dirFile.includes('.')) filelist = [...filelist, path.basename(dirFile, '.txt')]
@@ -34,11 +43,34 @@ class EasyDoc {
         return filelist;
     }
 
-    private getEditorText(length: number) {
-        let regex = new RegExp('(\###)')
-        let test = this.document.getWordRangeAtPosition(this.document.positionAt(2), regex);
-        console.log(test);
-        // this.document.getText(3)
+    private getPackageJSON(): any {
+        let json = JSON.parse(fs.readFileSync(this.dir + '/package.json', 'utf-8'));
+        return json;
+    }
+
+    private writeConfig(fileName) {
+        let packageJSON = this.getPackageJSON();
+        packageJSON.contributes.configuration.properties['EasyDoc.' + fileName] = {
+            "type": "object",
+            "default": {
+                "triggerString": "$$$",
+                "docType": "function"
+            }
+        };
+
+        fs.writeFile(this.dir + '/package.json', JSON.stringify(packageJSON, null, 4), (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        })
+    }
+
+    private getEditorText(triggerText: string): string {
+        let cursorPostition = this.document.selection.active;
+        let cursorLine = this.document.document.lineAt(cursorPostition['line']);
+        let search = cursorLine['text'].substring(cursorPostition['character'] - triggerText.length, cursorPostition['character']);
+        return search;
     }
 }
 
