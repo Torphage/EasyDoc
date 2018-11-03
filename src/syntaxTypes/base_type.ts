e48379d9614922eaf015cae8fdbed90743db1daf
Torphage <filip.torphage1@gmail.com>
2018-11-03 17:38:17 +0100

    moved functions, added potential fix for unescaping text

--- src/syntaxTypes/base_type.ts
+++ src/syntaxTypes/base_type.ts
@@ -21,6 +21,7 @@ export abstract class BaseSyntaxType {
 
     protected removeEscapeCharacters(text: string): string {
         let unescapedString = "";
+        text = text.replace(/\\\\\\\}/g, "}");
 
         for (let i = 0; i < text.length; i++) {
             if (text[i] === "\\") {
@@ -36,51 +37,7 @@ export abstract class BaseSyntaxType {
         return unescapedString;
     }
 
-    protected getCurrentLine(syntaxText: string, index: number): string {
-        const leftOfCurrentPos = syntaxText.slice(0, index);
-        const leftIndex = leftOfCurrentPos.lastIndexOf("\n");
-
-        const rightOfCurrentPos = syntaxText.slice(index);
-        const rightIndex = rightOfCurrentPos.indexOf("\n");
-
-        let fullLine: string;
-
-        if (rightIndex !== -1) {
-            fullLine = syntaxText.substring(leftIndex, leftOfCurrentPos.length + rightIndex);
-        } else {
-            fullLine = syntaxText.substring(leftOfCurrentPos.length + 2);
-        }
-
-        return fullLine.trim();
-    }
-
-    protected typeInLine(text: string) {
-        for (const key in this.vars) {
-            if (text.includes(`\${${key}}`) && !(text.includes(`\\\${${key}}`))) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    protected maxNumOfType(vars: any[]): any {
-        let maxRepeaters = 0;
-
-        vars.forEach((locals) => {
-            if (typeof locals !== "string") {
-                const variable = this.vars[locals.text.slice(2, -1)];
-
-                if (variable.length > maxRepeaters) {
-                    maxRepeaters = variable;
-                }
-            }
-        });
-
-        return maxRepeaters;
-    }
-
-    protected abstract applyType(text: string, alreadyEscaped?: boolean): vs.SnippetString;
+    protected abstract applyType(text: string): vs.SnippetString;
     protected abstract getType(types: ISyntaxType[], index: number): ISyntaxType;
     protected abstract getTypeText(types: ISyntaxType): any;
 }
