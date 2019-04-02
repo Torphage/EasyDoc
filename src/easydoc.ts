1a8a811a2d06e6be74eb4b67888b2030afaa10d8
Torphage <filip.torphage1@gmail.com>
2019-04-02 21:07:42 +0200

    first few comments

--- src/easydoc.ts
+++ src/easydoc.ts
@@ -92,12 +92,23 @@ export class EasyDoc {
         return fileConfig;
     }
 
+    /**
+     * Initialize and shows a QuickPick menu. This QuickPick will
+     * show different files withing the dir attribute of config.
+     * @param {Config} config he configuration of EasyDoc.
+     * @returns {Thenable<string>} A promise that resolves to the selection.
+     */
     private quickPick(config: Config): Thenable<string> {
         const items = this.getAllItems(config);
         const pick = vs.window.showQuickPick(items);
         return pick;
     }
 
+    /**
+     * Get every file's name and diretory inside a given dir based on the configuration.
+     * @param {Config} config The configuration of EasyDoc.
+     * @returns {string[]} List of file's name and directory.
+     */
     private getAllItems(config: Config): string[] {
         const items: string[] = [];
 
@@ -120,6 +131,11 @@ export class EasyDoc {
         return items;
     }
 
+    /**
+     * Get the text of the editor in front of the cursor, with the length of the triggerText.
+     * @param {string} triggerText The text that the length of the result will be.
+     * @returns {string} A string before the cursor with the length of triggerText..
+     */
     private getEditorText(triggerText: string): string {
         const cursorPostition = this.document.selection.active;
         const cursorLine = this.document.document.lineAt(cursorPostition.line);
