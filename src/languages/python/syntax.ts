30cb0e3a0c192f280f01763499ac2bc6a021c8ea
Torphage <filip.torphage1@gmail.com>
2019-04-03 15:57:10 +0200

    added syntax

--- src/languages/python/syntax.ts
+++ src/languages/python/syntax.ts
@@ -1,16 +1,41 @@
+/**
+ * Implementation of Python
+ */
 import { ISyntaxVariable } from "../../interfaces";
 import { WorkShop } from "../workshop";
 import { PythonParse } from "./parse";
 
+/**
+ * Python, extends WorkShop.
+ *
+ * @export
+ * @class Python
+ * @extends {WorkShop}
+ */
 export class Python extends WorkShop {
     protected parse: PythonParse;
 
+    /**
+     * Creates an instance of Python.
+     *
+     * @param {string} syntaxFile
+     * @param {string} docType
+     * @memberof Python
+     */
     constructor(syntaxFile: string, docType: string) {
         super(syntaxFile);
         this.parse = new PythonParse(docType);
     }
 
-    public correctlyPlacedFunction(functionLineIndex: string): boolean {
+    /**
+     * Check if the cursor is correctly placed in the function.
+     *
+     * @protected
+     * @param {string} functionLineIndex The row to search on.
+     * @returns {boolean} If cursor is correctly placed.
+     * @memberof WorkShop
+     */
+    protected correctlyPlacedFunction(functionLineIndex: string): boolean {
         const regex = /^\s*def\s/g;
 
         if (regex.exec(functionLineIndex) !== null) {
@@ -20,7 +45,14 @@ export class Python extends WorkShop {
         }
     }
 
-    public getVariables(): Promise<ISyntaxVariable> {
+    /**
+     * Get the variables based on the language.
+     *
+     * @protected
+     * @returns {Promise<ISyntaxVariable>} An promise to return the variables.
+     * @memberof WorkShop
+     */
+    protected getVariables(): Promise<ISyntaxVariable> {
         const parse = this.parse.parse(this.block);
         const params = this.parse.parseParams(parse.params);
 
@@ -39,7 +71,15 @@ export class Python extends WorkShop {
         });
     }
 
-    public getCurrentColumn(index: number): number {
+    /**
+     * Get the current column in the editor
+     *
+     * @protected
+     * @param {number} index The line index.
+     * @returns {number} The column the user is positioned at.
+     * @memberof WorkShop
+     */
+    protected getCurrentColumn(index: number): number {
         const leftOfCurrentPos = this.syntaxFile.slice(0, index);
         const leftIndex = leftOfCurrentPos.lastIndexOf("\n");
         const currentColumn = leftOfCurrentPos.length - leftIndex;
