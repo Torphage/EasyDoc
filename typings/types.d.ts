e1ac482f7228fe86c3a0b5e5ac1ab810f78ea8d1
Torphage <filip.torphage1@gmail.com>
2019-04-23 17:28:36 +0200

    updated docs for custom String functions

--- typings/types.d.ts
+++ typings/types.d.ts
@@ -1,19 +1,32 @@
 export {};
 
 declare global {
-    interface String {
+
+    /**
+     * The String type in Typescript.
+     *
+     * @interface String
+     */
+    interface String {       
+
         /**
-          * Returns the position of the first occurrence of a regular expression pattern.
-          * @param regexp The regular expression pattern to search for in the string
-          * @param position The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
-          */        
+         * Returns the position of the first occurrence of a regular expression pattern.
+         *
+         * @param {RegExp} regexp The regular expression pattern to search for in the string.
+         * @param {number} [position] The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
+         * @returns {number}
+         * @memberof String
+         */
         regexIndexOf(regexp: RegExp, position?: number): number;
 
         /**
-          * Returns the last occurrence of a regular expression pattern in the string.
-          * @param regexp The regular expression pattern to search for.
-          * @param position The index at which to begin searching. If omitted, the search begins at the end of the string.
-          */
+         * Returns the last occurrence of a regular expression pattern in the string.
+         *
+         * @param {RegExp} regexp The regular expression pattern to search for.
+         * @param {number} [position] The index at which to begin searching. If omitted, the search begins at the end of the string.
+         * @returns {number}
+         * @memberof String
+         */
         regexLastIndexOf(regexp: RegExp, position?: number): number
     }
 }
